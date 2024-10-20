45e82d116e4c39a20a066e673103c9e8a5017315
Vitaliy Boyarsky <boyarsky.vitaliy@live.com>
2024-10-20 14:49:12 +0300

    Added a script to easy deploy

---
+++ semver-api/deploy.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def pop(arg, required=True):
+    if '--' + arg not in argv:
+        if required:
+            raise ValueError(f'--{arg} is required')
+        return None
+
+    index = argv.index('--' + arg)
+    indexes = [index]
+    values = []
+    while True:
+        index += 1
+        if index >= len(argv) or argv[index].startswith('--'):
+            break
+
+        values.append(argv[index])
+        indexes.insert(0, index)
+
+    for index in indexes:
+        argv.pop(index)
+
+    return ' '.join(values)
+
+
+def discover_version():
+    # TODO: write a function to discover the version from the project. read the version from a file, or from the git tags/branches
+    pass
+
+
+argv = sys.argv[1:]
+
+stack_name = pop('stack-name')
+template_file = pop('template-file').removesuffix('api.yaml')
+parameter_overrides = pop('parameter-overrides')
+
+major, minor, patch = (pop('version') or discover_version()).split('.')
+update_latest = pop('no-update-latest', False) is None
+
+cmd = (
+    'aws cloudformation deploy',
+    '--stack-name', stack_name + '-api',
+    '--template-file', template_file + 'api.yaml',
+    '--parameter-overrides', parameter_overrides,
+    *argv
+)
+print(' '.join(cmd))
+
+cmd = (
+    'aws cloudformation deploy',
+    '--stack-name', stack_name + f'-api-v{major}-{minor}',
+    '--template-file', template_file + 'api-semver.yaml',
+    '--parameter-overrides', parameter_overrides, f'Major={major}', f'Minor={minor}', f'Patch={patch}',
+    *argv
+)
+print(' '.join(cmd))
+
+cmd = (
+    'aws cloudformation deploy',
+    '--stack-name', stack_name + '-api-latest',
+    '--template-file', template_file + 'api-semver-latest.yaml',
+    '--parameter-overrides', parameter_overrides, f'Major={major}', f'Minor={minor}', f'Patch={patch}',
+    *argv
+)
+
+if update_latest:
+    print(' '.join(cmd))
